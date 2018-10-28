574ba2a0545574b17f1344286f7b8ca272b55534
LAPTOP-LK4OIR7G\niek_ <niek_riggeling@hotmail.com>
2018-10-28 11:22:53 +0100

    JS learning new commit

---
+++ celcius.js
@@ -0,0 +1,25 @@
+var score = 100
+
+score = 130
+
+const bonus = 50
+
+var finalScore = score + bonus * 1.8
+
+var complex = ((4 + 4) * 5) / 2
+// Haakjes gebruiken voor berekeningen
+
+// console.log(finalScore)
+// console.log(complex)
+
+let tempInFahrenheit = 100
+
+// Calculation
+// (T(F) - 32) x 5 / 9
+
+let celcius = (tempInFahrenheit - 32) * 5/9 
+
+
+
+console.log(celcius)
+
---
+++ variables.js
@@ -0,0 +1,17 @@
+let name = 'Peter'
+
+let score = 103
+
+let bonus = 20
+
+let totalScore = score + bonus
+
+console.log(totalScore);
+
+let firstName = 'Pieter'
+
+let lastName = 'de Vries'
+
+console.log(firstName + ' ' + lastName)
+
+firstName = 'Pieter'
