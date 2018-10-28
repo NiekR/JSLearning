82d2993b3571c29b4d4ab54a20f5c28b9d3a986e
nickcraw21 <niekriggeling@gmail.com>
2018-10-28 11:01:40 +0100

    Js learning first commit

---
+++ nullandBool.js
@@ -0,0 +1,19 @@
+//temp coming from website API
+
+var temp 
+
+//temp = 2
+
+console.log("Current temperature is: " + temp)
+
+//Grade system
+
+// 10 - great
+// 5 -  mediocre
+// 2 - poor
+// 0 -fail
+
+let grade = true 
+
+console.log(grade)
+
