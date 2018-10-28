772a258ebcb60b80e7c9d6d6346df94950eec477
LAPTOP-LK4OIR7G\niek_ <niek_riggeling@hotmail.com>
2018-10-28 19:44:36 +0100

    king territory, array counting, js in string

---
+++ marvels.js
@@ -0,0 +1,15 @@
+//datatype - Array
+
+const superHeroes = ['Iron man', 'Spiderman', 'Captain America']
+
+console.log(superHeroes);
+
+// Call first item in array
+console.log(superHeroes[0]);
+
+// Call last item in array
+console.log(superHeroes[superHeroes.length - 1]);
+
+// Tildes under ESC
+console.log(`We have ${superHeroes.length} super heroes `)
+
--- scope.js
+++ scope.js
@@ -1,4 +1,4 @@
-// Scope is code inside { } 
+/*// Scope is code inside { } 
 // Var makes variable global from scope Let makes it local only inside scope
 
 let iAmGlobal = 'value'
@@ -13,3 +13,21 @@ if (true) {
 console.log(iamLocal);
 console.log(iAmGlobal);
 
+// */
+
+// Kings Problems
+
+//let king = 'Peter'
+
+if (true) {
+	//let king = 'Sam'
+
+	if (true) {
+		let king = 'Ram'
+		console.log(king);
+	}
+}
+
+if (true){
+	console.log("I am second part : " + king);
+}
