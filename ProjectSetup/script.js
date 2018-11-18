b0eba7562835778d50f2d8aaab383f45c8f3e032
LAPTOP-LK4OIR7G\niek_ <niek_riggeling@hotmail.com>
2018-11-18 20:24:58 +0100

    setAttribute, appendChild

---
+++ ProjectSetup/script.js
@@ -0,0 +1,65 @@
+// var h = document createElement('h1')
+
+// // Textnode
+// var myValue = document.createTextNode('Wattup')
+
+// h.appendChild(myValue)
+
+// document.querySelector('h1').appendChild(h)
+
+
+// While
+let val = 5;
+
+var ul = document.getElementById('list')
+var li;
+
+var addButton = document.getElementById('add');
+addButton.addEventListener('click', removeItem);
+
+
+while (val > 0) {
+	// statement
+	console.log(val)
+    //val = val -1
+	val--
+
+}
+
+function addItem(){
+	var input = document.getElementById('input');
+	var item = input.value;
+	ul = document.getElementById('list');
+	var Textnode = document.createTextNode(item)
+
+	if (item === '') {
+		return false
+	} else{
+       // create li, checkbox and label
+       li = document.createElement('li');
+       
+       var checkbox = document.createElement('input')
+       checkbox.type = 'checkbox';
+
+       checkbox.setAttribute('íd', 'check');
+
+       var label = document.createElement('label');
+       label.setAttribute('for', 'item');
+
+       //add elements to page
+       ul.appendChild(label);
+       li.appendChild(checkbox);
+       label.appendChild(textnode);
+       li.appendChild(label);
+
+       // What to add, where to add it
+       ul.insertBefore(li, ul.childNodes[0]);
+
+       li.className = 'visual';
+
+
+
+
+
+	}
+}
--- WebBased/validation.js
+++ WebBased/validation.js
@@ -1,12 +1,23 @@
+const myPElements = document.querySelectorAll('p')
+
 // validation
 function myValidation () {
 	let myValue = document.getElementById('myform').value;
 
 	// isNan = return true is argument is NotANumber 
 	if (isNaN(myValue) || myValue < 1 || myValue > 20) {
-		console.log('Not a valid input');
+		myPElements.forEach(p => { p.textContent = 'Valid input of 0 - 20'});
 	} else {
-		console.log('Valid input');
+		myPElements.forEach(p => { p.textContent = 'Invalid input of greater than 20'});
 	}
 
 }
+
+// form validation w/out url
+
+document.querySelector('.myform').addEventListener('submit', (event)=>{
+	event.preventDefault();
+	console.log(event.target.username.value)
+	console.log(event.target.firstname.value)
+	event.target.username.value = ''
+})
