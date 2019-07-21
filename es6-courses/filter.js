fd88ba8f250d64f790ab52e4c675ac9fc61eafb4
LAPTOP-LK4OIR7G\niek_ <niek_riggeling@hotmail.com>
2019-07-21 16:55:18 +0200

    filters continued

--- es6-courses/filter.js
+++ es6-courses/filter.js
@@ -1,24 +1,48 @@
-// Filter helper
-var products = [
-    { name: 'cucumber', type: 'vegetable' },
-    { name: 'banana', type: 'fruit' },
-    { name: 'bread', type: 'bakery' }
+// // Filter helper
+// var products = [
+//     { name: 'cucumber', type: 'vegetable', quantity: 5, price: 5 },
+//     { name: 'banana', type: 'fruit',quantity: 10, price: 8 },
+//     { name: 'bread', type: 'bakery',quantity: 20, price: 25 }
+// ];
+//
+// var filteredProducts = [];
+//
+// for (var i = 0; i < products.length; i++){
+//     if (products[i].type === 'fruit'){
+//         filteredProducts.push(products[i]);
+//     }
+// }
+//
+// console.log(filteredProducts);
+//
+// var newFilteredProducts = products.filter(function(product){
+//     return product.type === 'fruit';
+// });
+//
+// console.log(newFilteredProducts);
+//
+// // Type is vegetable quantity greater than 0  price less than then 10
+//
+// var specificFilteredProducts = products.filter(function(product){
+//    return product.type === 'vegetable'
+//        && product.quantity > 0
+//        && product.price < 10;
+// });
+//
+// console.log(specificFilteredProducts);
+
+var post = {id: 4, title: 'new post'};
+
+var comments = [
+    { postId: 4, content: 'Nice' },
+    { postId: 3, content: 'Good job' },
+    { postId: 4, content: 'This sucks' },
 ];
 
-var filteredProducts = [];
-
-for (var i = 0; i < products.length; i++){
-    if (products[i].type === 'fruit'){
-        filteredProducts.push(products[i]);
-    }
+function commentsForPost(post, comments) {
+    return comments.filter(function(comment){
+       return comment.postId === post.id;
+    });
 }
 
-console.log(filteredProducts);
-
-var newFilteredProducts = products.filter(function(product){
-    return product.type === 'fruit';
-});
-
-console.log(newFilteredProducts);
-
-
+console.log(commentsForPost(post, comments));
